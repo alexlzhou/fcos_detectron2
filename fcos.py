4aa83f3b3ff4486e5202712191575162e89a46c2
Alex Zhou <alex.lawsonzhou@hotmail.com>
2021-10-27 23:25:43 -0400

    training initial

--- fcos.py
+++ fcos.py
@@ -10,7 +10,7 @@ import os
 from detectron2.model_zoo import model_zoo
 from detectron2.utils.visualizer import Visualizer
 
-'''
+
 register_coco_instances("my_dataset_train",
                         {},
                         "D:/projects_python/_datasets/coco_2017/annotations/instances_train2017.json",
@@ -23,8 +23,37 @@ register_coco_instances("my_dataset_val",
 
 metadata = MetadataCatalog.get("my_dataset_train")
 dataset_dicts = DatasetCatalog.get("my_dataset_train")
-'''
 
+cfg = get_cfg()
+cfg.merge_from_file(model_zoo.get_config_file("COCO-InstanceSegmentation/mask_rcnn_R_50_FPN_3x.yaml"))
+cfg.DATASETS.TRAIN = ("my_dataset_train",)
+cfg.DATASETS.TEST = ()
+cfg.DATALOADER.NUM_WORKERS = 2
+cfg.MODEL.WEIGHTS = model_zoo.get_checkpoint_url("COCO-InstanceSegmentation/mask_rcnn_R_50_FPN_3x.yaml")
+# cfg.SOLVER.IMS_PER_BATCH = 2
+# cfg.SOLVER.BASE_LR = 0.00025
+# cfg.SOLVER.MAX_ITER = 300
+# cfg.MODEL.ROI_HEADS.BATCH_SIZE_PER_IMAGE = 512
+# cfg.MODEL.ROI_HEADS.NUM_CLASSES = 5
+
+os.makedirs(cfg.OUTPUT_DIR, exist_ok=True)
+trainer = DefaultTrainer(cfg)
+trainer.resume_or_load(resume=False)
+trainer.train()
+
+cfg.MODEL.WEIGHTS = os.path.join(cfg.OUTPUT_DIR, "model_final.pth")
+cfg.MODEL.ROI_HEADS.SCORE_THRESH_TEST = 0.5
+cfg.DATASETS.TEST = ("my_dataset_val", )
+predictor = DefaultPredictor(cfg)
+
+im = cv2.imread("D:/projects_python/_datasets/coco_2017/train2017/000000000036.jpg")
+outputs = predictor(im)
+v = Visualizer(im[:, :, ::-1], MetadataCatalog.get(cfg.DATASETS.TRAIN[0]), scale=1.2)
+out = v.draw_instance_predictions(outputs["instances"].to("cpu"))
+cv2.imshow('', out.get_image()[:, :, ::-1])
+cv2.waitKey(0)
+
+'''
 im = cv2.imread("D:/projects_python/_datasets/coco_2017/train2017/000000000036.jpg")
 
 cfg = get_cfg()
@@ -43,3 +72,4 @@ v = Visualizer(im[:, :, ::-1], MetadataCatalog.get(cfg.DATASETS.TRAIN[0]), scale
 out = v.draw_instance_predictions(outputs["instances"].to("cpu"))
 cv2.imshow('', out.get_image()[:, :, ::-1])
 cv2.waitKey(0)
+'''
